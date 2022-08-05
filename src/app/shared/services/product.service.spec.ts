780d72c0c4ea775442fed49255d8493b976d4479
SachinGaikwadMB <sachin.gaikwad@mindbowser.com>
2022-08-05 16:42:08 +0530

    unit-test-cases

--- src/app/shared/services/product.service.spec.ts
+++ src/app/shared/services/product.service.spec.ts
@@ -1,17 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 import { ProductService } from './product.service';
 
 describe('ProductService', () => {
   let service: ProductService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    providers: [ProductService];
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers:[ProductService]
+    });
     service = TestBed.inject(ProductService);
   });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  it('should have getAllProducts function', () => {
+    const service: ProductService = TestBed.get(ProductService);
+    expect(service.getAllProducts).toBeTruthy();
+   });
+
 });
